909d85f2e98881bdc022f3d301a5ed0e1c9a812d
XXXXXX730 <XXXXXX730@proton.me>
2025-10-11 23:37:48 +0100

    Fourth Commit

--- 2_scripts/09.10.2025.first.session.R
+++ 2_scripts/09.10.2025.first.session.R
@@ -69,6 +69,8 @@ mouse.info[, c("weights", "colours")]
 # Question 3 Call up the help file for mtcars. What does the data in the column “drat” refer to?
 ?mtcars
   
+  help(mtcars) # this is also an alternative
+  
   #[, 5]	drat	Rear axle ratio
   
   
@@ -77,6 +79,10 @@ mouse.info[, c("weights", "colours")]
   
   
   horsepower <- mtcars$hp
+  horsepower[10]
+  # alternatives
+  
+  horsepower <- mtcars[,'hp']
   
   
  # Question 5
@@ -85,9 +91,17 @@ mouse.info[, c("weights", "colours")]
   
 mtcars$mpg[32]  # first option
 mtcars[32,1]    # Second option
-mtcars$mpg[length(mtcars$mpg)]
+
 
 mtcars$mpg[dim(mtcars)[1]]
+
+
+#if you want to create a script that would always generate the final 
+#element regardless the changing the number of elements
+
+mtcars$mpg[nrow(mtcars)]
+mtcars$mpg[length(mtcars$mpg)]
+
 
 # Second session 
 
@@ -102,13 +116,124 @@ property
 
 print(property)
 
-?print
-??width
+
+#
+
+## Sumarizing data-----------------------------------------------------
+
+
+mtcars$cyl <- factor(mtcars$cyl)
+mtcars$am <- factor(mtcars$am, labels = c('auto','manual'))
+
+
+boxplot(mpg~cyl, data=mtcars)
 
 
 
-temcars <- mtcars
+boxplot(mpg~am*cyl, data=mtcars)
 
-temcars$hp <- as.character(temcars$hp)
-temcars
-class(temcars$hp);class(mtcars$hp)
+
+hist(mtcars$mpg)
+
+hist(mtcars$mpg, breaks = 15)  # gives the number of columns we desire to see
+
+
+
+
+# scatter plot
+
+plot(mpg~disp, data = mtcars)
+
+
+pairs(~mpg+disp+vs, data = mtcars)
+
+
+
+# tables based visuals of the data
+
+
+
+
+xtabs(~am, data = mtcars) # only for categorical data sets
+
+
+xtabs(~am+cyl, data=mtcars)
+
+
+
+prop.table(xtabs(~am, data=mtcars), margin=1)
+
+prop.table(xtabs(~am+cyl, data=mtcars), margin=1)
+
+prop.table(xtabs(~am+cyl, data=mtcars))
+
+#aggregate()
+
+
+aggregate(mpg~am, data=mtcars, mean)
+
+aggregate(mpg~am+cyl, data = mtcars, mean)
+
+
+aggregate(cbind(mpg,disp)~am, data = mtcars, mean)
+
+
+
+
+## practical 2----------------------------------------
+
+
+# 
+# Question 1
+# How many guinea pigs received each dose?
+
+xtabs(~dose, data=ToothGrowth)
+
+#   
+#   How many received each supplement type?
+
+xtabs(~supp, data=ToothGrowth)
+
+
+#   
+#   How many received each combination of dose and supplement type?
+
+
+xtabs(~supp+dose, data=ToothGrowth)
+
+# 
+# Question 2
+# What proportion of total guinea pigs received each combination of dose and supplement type?
+
+prop.table(xtabs(~supp+dose, data=ToothGrowth))
+# 
+# Question 3
+# What was the maximum tooth length recorded for each dose?
+
+summary(ToothGrowth$len)
+max(ToothGrowth$len)
+
+#   
+#   For each combination of dose and supplement type, what is the sum of all tooth lengths?
+
+prop.table(cbind())
+
+
+
+# Question 4
+# Draw a boxplot of tooth length for each dose of vitamin C.
+# 
+boxplot(len~dose, data=ToothGrowth)
+# Draw a boxplot of tooth length for each combination of dose and 
+# supplement type, with the first two boxplots showing dose 0.5, the 
+# following two boxplots showing dose 1.0 and the final two plots showing 
+# dose 2.0.
+boxplot(len~dose*supp, data=ToothGrowth)
+# 
+
+
+# Question 5
+# Create a histogram of tooth length, containing more than 10 columns or “breaks”.
+
+
+hist(ToothGrowth $ len, data= ToothGrowth, breaks= 14)
--- 2_scripts/10.10.2025.second.session.R
+++ 2_scripts/10.10.2025.second.session.R
@@ -1,34 +1,136 @@
 # 2nd session 10.10.2025--------------------------------------------------------
 
+
+
+# Data class in R 
+
+
+
 id <- c("REF001", "REF002", "REF003", "REF004", "REF005", "REF006")
 house.prices <- c(100, 200, 1000, 50, 500, 500)
 no.bedrooms <- c(1, 2, 5, 1, 3, 2)
 parking <- c("Yes", "Yes", "Yes", "No", "Yes", "Yes")
 
-property <- data.frame(id,house.prices,no.bedrooms,parking)
+
+class(id)
+
+reason <- factor(c("Too many side effects", "Moved house", "Unknown"))
+
+reason <- as.character(reason)
+
+class(reason)
+
+
+housing.info <- data.frame(id=id, price=house.prices, 
+                          bedroom=no.bedrooms, parking=parking)
+
+
+dim(housing.info)
+
+head(housing.info)
+
+ncol(housing.info)
+nrow(housing.info)
+
+housing.info[3,3]
+
+housing.info[,1]
+
+housing.info[3,]
+
+
+# if the data set is large and its difficult to know the position of col of a given variable
+
+housing.info[,'parking']
+
+# this is the same as;
+
+housing.info$parking
+
+# if we need to call multiple columns
+
+housing.info[,c('price','bedroom')]
+
+housing.info[,1:3]
+
+# if the columns or rows that we want to call arent consecutive we use c()
+
+housing.info[c(1,5),c(1,3:4)]
 
 #renaming the colnames
 
-colnames(property) <- c('ID','price','beds','park')
+#always use the comments to rememeber how your code behaves
+
+# expanding data frames
+
+
+id <- c("REF001", "REF002", "REF003", "REF004", "REF005", "REF006")
+house.prices <- c(100, 200, 1000, 50, 500, 500)
+no.bedrooms <- c(1, 2, 5, 1, 3, 2)
+parking <- c("Yes", "Yes", "Yes", "No", "Yes", "Yes")
+
+
+property <- data.frame(id,house.prices, no.bedrooms, parking)
+
+
+
+# changing the column names and row names
+
+colnames(property) <- c('ID', 'price', 'beds', 'park') 
+
+# its also possible to create the name of the columns when creating the data frame, 
+#by puting the names of the columns as arguemnents
+
+
+
+property <- data.frame(ID=id, price=house.prices, beds=no.bedrooms, park=parking)
+
+
+# if you need to change the name of a single column
+
+
+
+
+colnames(property)[2] <- 'price.usd'
+ 
+
+colnames(property)[2] <- 'price'
 
 
 rownames(property) <- c("houseA","houseB","houseC","houseD","houseE","houseF")
 
 
-# COLUMN  BIND
+# you can then call the row of specific name
 
-property <- cbind(property, bathroom=c()
-
-
+property["houseA","price"]
 
 
+# adding column to the existing data set
+
+property$type <- c("terrace", "terrace", "detached", "flat", "detached", 
+                   "detached")
 
 
+property <- cbind(property, bathrooms=c(1, 1, 3, 1, 2, 2),
+                  city.centre=c("No", "No", "Yes", "No", "Yes", "No") )
+
+property <- rbind(property,houseG = c('REF007', 578,4,'No','detached',2,'No'))
+
+# the merge function
 
 
+property.partA <- property[,1:4]
+property.partA
+property.partB <- property[3:6, c(1,2,4:6)]
+
+property.partB
 
 
+merge(property.partA, property.partB, all=T, )
 
+?merge
+
+merge(property.partA, property.partB, by = 'id', all=F)
 
 
 
